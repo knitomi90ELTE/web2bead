fb43efa56441a645f3b5dd1d743ae4d40f385190
Knizner Tamás <knitomi90@inf.elte.hu>
2016-04-01 22:50:54 +0200

    scrolls

--- public_html/game.js
+++ public_html/game.js
@@ -1,33 +1,36 @@
 window.addEventListener('load', init, false);
 
 var CONSTANTS = {
-    DEFAULT_ROW_COUNT : 10,
-    DEFAULT_COL_COUNT : 10,
-    DEFAULT_OBS_COUNT : 2,
-    BASE_INTERVAL : 400,
-    OBSTACLE_IMAGE : '',
-    SNAKE_START : {
-        x :0,
+    DEFAULT_ROW_COUNT: 10,
+    DEFAULT_COL_COUNT: 10,
+    DEFAULT_OBS_COUNT: 2,
+    BASE_INTERVAL: 400,
+    SNAKE_START: {
+        x: 0,
         y: 0
     }
 };
 
 var game = {
-    running : null,
-    logging : false,
-    round : 1,
-    score : 1,
-    runnningInterval : CONSTANTS.BASE_INTERVAL
+    running: null,
+    logging: false,
+    round: 1,
+    score: 1,
+    runnningInterval: CONSTANTS.BASE_INTERVAL
 };
 
 var table = {
-    tekercs : null,
-    obstacles : []
+    scrollOnTable: {
+        type: null,
+        position: null
+    },
+    obstacles: [],
+    mirrorEffect: false
 };
 
 var snake = {
-    direction : 0,
-    cells : []
+    direction: 0,
+    cells: []
 };
 /*
  0 -> jobbra
@@ -35,7 +38,75 @@ var snake = {
  2 -> balra
  3 -> le
  */
-
+/*
+ Bölcsesség tekercse (80%): a sárkány 4 egységgel növekszik tőle.
+ Tükrök tekercse (4%): az irányítás tükörképben történik irányonként (fel helyett le, bal helyett jobb).
+ Fordítás tekercse (4%): a sárkány haladási iránya megfordul (feje és farka helyett cserél).
+ Mohóság tekercse (4%): a sárkány haladási sebessége másfélszeresére nő 5 másodpercig.
+ Lustaság tekercse (4%): a sárkány haladási sebessége másfélszeresére csökken 5 másodpercig.
+ Falánkság tekercse (4%): a sárkány 10 egységgel növekszik tőle.
+ */
+var scrolls = {
+    wisdom: {
+        name: 'Bölcsesség tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            extendSnake(4);
+        }
+    },
+    mirror: {
+        name: 'Tükrök tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            table.mirrorEffect = true;
+        }
+    },
+    reverse: {
+        name: 'Fordítás tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            switch (snake.direction) {
+                case 0:
+                    snake.direction = 2;
+                    break;
+                case 1:
+                    snake.direction = 3;
+                    break;
+                case 2:
+                    snake.direction = 0;
+                    break;
+                case 3:
+                    snake.direction = 1;
+                    break;
+            }
+        }
+    },
+    greedy: {
+        name: 'Mohóság tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            game.runnningInterval *= 1.5;
+            pauseGame();
+            startGame();
+        }
+    },
+    lazy: {
+        name: 'Lustaság tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            game.runnningInterval /= 1.5;
+            pauseGame();
+            startGame();
+        }
+    },
+    voracious: {
+        name: 'Falánkság tekercse',
+        execute: function () {
+            if(game.logging) console.log('Executing ' + this.name);
+            extendSnake(10);
+        }
+    }
+};
 var colCount;
 var rowCount;
 var obsCount;
@@ -45,19 +116,23 @@ var tukrokTekercse = false;
 document.onkeydown = function (e) {
     switch (e.keyCode) {
         case 37:
-            if(game.logging) console.log('left');
+            if (game.logging)
+                console.log('left');
             snake.direction = tukrokTekercse ? 0 : 2;
             break;
         case 38:
-            if(game.logging) console.log('up');
+            if (game.logging)
+                console.log('up');
             snake.direction = tukrokTekercse ? 3 : 1;
             break;
         case 39:
-            if(game.logging) console.log('right');
+            if (game.logging)
+                console.log('right');
             snake.direction = tukrokTekercse ? 2 : 0;
             break;
         case 40:
-            if(game.logging) console.log('down');
+            if (game.logging)
+                console.log('down');
             snake.direction = tukrokTekercse ? 1 : 3;
             break;
     }
@@ -70,56 +145,103 @@ function init() {
     $('#newGame').addEventListener('click', resetData, false);
 }
 
-function getRandomCell(){
+function getRandomCell() {
     var coord = {
-        x : Math.floor(Math.random() * rowCount),
-        y : Math.floor(Math.random() * colCount)
+        x: Math.floor(Math.random() * rowCount),
+        y: Math.floor(Math.random() * colCount)
     };
-    return (!isSnake(coord) || !isObstacle(coord)) ? coord : getRandomCell();
+    return (isSnake(coord) || !isObstacle(coord)) ? coord : getRandomCell();
 }
 
 function getColumn(x, y) {
-    if(game.logging) console.log('getColumn',x, y, game.round);
+    if (game.logging)
+        console.log('getColumn', x, y, game.round);
     return $('#gameTable').rows[x].cells[y];
 }
 
-function changeColumnColor(cell, color){
-    try{
+function changeColumnColor(cell, color) {
+    try {
         getColumn(cell.x, cell.y).style.backgroundColor = color;
-    }catch (e){
-        if(game.logging) console.log('Nincs ilyen cella');
+    } catch (e) {
+        if (game.logging)
+            console.log('Nincs ilyen cella');
     }
 }
 
 function colorColumn(cell) {
-    if(game.logging) console.log('colorColumn',cell.x, cell.y, game.round);
+    if (game.logging)
+        console.log('colorColumn', cell.x, cell.y, game.round);
     changeColumnColor(cell, 'red');
 }
 
 function removeSnakeCell(cell) {
-    if(game.logging) console.log('removeSnakeCell',cell.x, cell.y, game.round);
+    if (game.logging)
+        console.log('removeSnakeCell', cell.x, cell.y, game.round);
     changeColumnColor(cell, '');
 }
 
+function extendSnake(value) {
+    game.score += value;
+    updateScoreLabel();
+}
+
 function drawSnake() {
-    if(game.logging) console.log('drawSnake',game.round);
+    if (game.logging)
+        console.log('drawSnake', game.round);
     snake.cells.forEach(colorColumn);
 }
 
 function initSnake() {
-    if(game.logging) console.log('initSnake',game.round);
+    if (game.logging)
+        console.log('initSnake', game.round);
     snake.cells = [CONSTANTS.SNAKE_START];
     snake.cells.forEach(colorColumn);
 }
 
-function initObstacles(){
+function initObstacles() {
     var k = $('#k').value;
-    for(var i = 0; i < k; i++){
+    for (var i = 0; i < k; i++) {
         var coord = getRandomCell();
         table.obstacles.push(coord);
-        if(game.logging) console.log('Obstacle at ', coord.x, coord.y);
-        getColumn(coord.x, coord.y).innerHTML = '<img src="images/obstacle.png" class="obstacle">';
+        if (game.logging)
+            console.log('Obstacle at ', coord.x, coord.y);
+        getColumn(coord.x, coord.y).innerHTML = '<img src="images/obstacle.png" class="sm-img">';
     }
+}
+
+/*
+0-79
+80-83
+84-87
+88-91
+92-95
+96-99
+ */
+function getScroll() {
+    var x = Math.floor(Math.random() * 100);
+    if (x < 80) {
+        return 'wisdom';
+    } else if (x < 84) {
+        return 'mirror';
+    } else if (x < 88) {
+        return 'reverse';
+    } else if (x < 92) {
+        return 'greedy';
+    } else if (x < 96){
+        return 'lazy';
+    } else {
+        return 'voracious';
+    }
+}
+
+
+function placeScroll() {
+    var coord = getRandomCell();
+    var type = getScroll();
+    table.scrollOnTable.position = coord;
+    table.scrollOnTable.type = scrolls[type];
+    getColumn(coord.x, coord.y).innerHTML = '<img src="images/' + type + '.png" class="sm-img">';
+    $('#scroll').innerHTML = type;
 }
 
 function clearTable() {
@@ -155,6 +277,15 @@ function pauseGame() {
     }
 }
 
+function updateScoreLabel(){
+    $('#score').innerHTML = 'Score: ' + game.score;
+}
+
+function disableButtons() {
+    $('#startGame').classList.add("disabled");
+    $('#pauseGame').classList.add("disabled");
+}
+
 function enableButtons() {
     $('#startGame').classList.remove("disabled");
     $('#pauseGame').classList.remove("disabled");
@@ -170,8 +301,9 @@ function generateTable() {
     $('#gameTable').innerHTML = generateHtml();
     initSnake();
     initObstacles();
+    placeScroll();
+    updateScoreLabel();
     enableButtons();
-
 }
 
 function generateHtml() {
@@ -186,7 +318,7 @@ function generateHtml() {
     return tableHtml;
 }
 
-function getNewSnakePosition(){
+function getNewSnakePosition() {
     var lastIndex = snake.cells.length - 1;
     var tmp;
     switch (snake.direction) {
@@ -207,13 +339,21 @@ function getNewSnakePosition(){
 }
 
 function moveSnake() {
-    if(game.logging) console.log('moveSnake',game.round);
+    if (game.logging)
+        console.log('moveSnake', game.round);
     var newPos = getNewSnakePosition();
     if (isPlaceNotOk(newPos)) {
-        if(game.logging) console.log('outOfBounds',game.round);
+        if (game.logging)
+            console.log('outOfBounds', game.round);
         gameOver();
         return;
     }
+    
+    if (isTheSamePosition(newPos, table.scrollOnTable.position)){
+        table.scrollOnTable.type.execute();
+    }
+
+
     removeSnakeCell(snake.cells[0]);
     snake.cells.shift();
     snake.cells.push(newPos);
@@ -221,55 +361,42 @@ function moveSnake() {
     game.round++;
 }
 
-function isPlaceNotOk(coord){
+function isPlaceNotOk(coord) {
     return isObstacle(coord) || isOut(coord);
 }
 
-function isOut(coord){
-    return coord.x < 0 || coord.x > colCount || coord.y < 0 || coord.y > rowCount;
+function isOut(coord) {
+    return coord.x < 0 || coord.x >= colCount || coord.y < 0 || coord.y >= rowCount;
 }
 
-function isObstacle(coord){
-    /*var isObs = false;
-    table.obstacles.map(function(o){
-        console.log(o);
-        if(coord.x === o.x && coord.y === o.y) {
-            isObs = true;
-            return;
-        };
-    });
-    return isObs;*/
-    return table.obstacles.indexOfObject(coord);
+function isObstacle(coord) {
+    return table.obstacles.indexOfObject(coord) !== -1;
 }
 
-function isSnake(coord){
-    /*var isSnk = false;
-    snake.cells.map(function(o){
-        console.log(o);
-        if(coord.x === o.x && coord.y === o.y) {
-            isSnk = true;
-            return;
-        };
-    });
-    return isSnk;*/
-    return snake.cells.indexOfObject(coord);
+function isSnake(coord) {
+    return snake.cells.indexOfObject(coord) !== -1;
 }
 
-Array.prototype.indexOfObject = function(obj){
+function isTheSamePosition(p1, p2){
+    return p1.x === p2.x && p1.y === p1.y;
+}
+
+Array.prototype.indexOfObject = function (obj) {
     var index = -1;
-    for(var i = 0; i < this.length; i++){
+    for (var i = 0; i < this.length; i++) {
         var objKeys = Object.keys(obj);
         var itmKeys = Object.keys(this[i]);
         var match = true;
-        for(var j = 0; j < itmKeys.length; j++){
+        for (var j = 0; j < itmKeys.length; j++) {
             var objKey = objKeys[j];
             var itmKey = itmKeys[j];
-            if(this[i][itmKey] !== obj[objKey]){
+            if (this[i][itmKey] !== obj[objKey]) {
                 match = false;
                 break;
             }
         }
-        if(match) index = i;
+        if (match)
+            index = i;
     }
     return index;
 };
--- public_html/mymodal.js
+++ public_html/mymodal.js
@@ -1,28 +1,16 @@
-// Get the modal
-var modal = $('#myModal');
+var myModal = $('#myModal');
 
-// Get the button that opens the modal
-var btn = $('#myBtn');
-
-// Get the <span> element that closes the modal
-var span = $('#closeBtn');
-
-// When the user clicks the button, open the modal 
 function showMyModal(message){
     $('#modal-message').innerHTML = message;
-    modal.style.display = "block";
+    myModal.style.display = "block";
 }
 
 function hideMyModal(){
-    modal.style.display = "none";
+    myModal.style.display = "none";
 }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = hideMyModal;
-
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target === modal) {
-        modal.style.display = "none";
+    if (event.target === myModal) {
+        myModal.style.display = "none";
     }
 };
